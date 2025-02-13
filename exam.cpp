f733a979a04bd7f9553ac0aa809e01bf75919e2a
princesojitra66 <princesojitra3914@gmail.com>
2025-02-13 19:23:21 +0530

    pr1

--- exam.cpp
+++ exam.cpp
@@ -3,27 +3,29 @@ using namespace std;
 int main()
 {
     int marks;
-    char grade = 'A';
+    char     grade = 'A';
     cout << " enter your marks :- ";
     cin >> marks;
-
+    (marks<0 && marks>100)?  
     (marks <= 100 && marks >= 85) ? cout << "your grade is A ."
     : (marks < 85 && marks >= 75) ? cout << "Your grade is B ."
     : (marks < 75 && marks >= 65) ? cout << "Your grade is C ."
     : (marks < 65 && marks >= 50) ? cout << "Your grade is D ."
-                                  : cout << "Your grade is F .";
+                                  : cout << "Your grade is F ."
+    :
+    cout << "invalid marks ";
 
     switch (grade)
     {
     case 'A':
     if(marks<=100 && marks>=85){
-        cout<<"Excellent work!";
+        cout<<"Excellent work!  ";
       }else if(marks<85 && marks>=75){
-          cout<<"Well done ";
+          cout<<"Well done. ";
       }else if(marks<75 && marks>=65){
-          cout<<"Good job  ";
+          cout<<"Good job.  ";
       }else if(marks<65 && marks>=50){
-          cout<<"You passed,but you could do better  ";
+          cout<<"You passed,but you could do better.  ";
       }else {
           cout<<"Sorry you failed .";
       }
@@ -41,6 +43,6 @@ int main()
     }
     else
     {
-        cout << "Please try again next time" << endl;
+        cout << "Please try again next time. " << endl;
     }
 }
